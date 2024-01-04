9839a373a89310f008b2ac65e412015f127c1799
sunil barewar <sunilvbarewar@gmail.com>
2024-01-04 13:03:47 +0530

    added home route and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
       <div className="px-[10px] lg:px-[80px] border border-red-800">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
       </div>
     </>
   );
