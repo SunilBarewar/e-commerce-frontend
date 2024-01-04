a03ee1799dd14f339a91542f79bc72bc31bc1593
sunil barewar <sunilvbarewar@gmail.com>
2024-01-04 12:54:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <div className="px-[10px] lg:px-[80px] border border-red-800">
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#db4444",
+        "dark-1": "#000000",
+        "dark-2": "#7d8184",
+        "white-1": "#ffffff",
+        "white-2": "#f5f5f5",
+        green: "#00d656",
+      },
+    },
+  },
+  plugins: [],
+};
