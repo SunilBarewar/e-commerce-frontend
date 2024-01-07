9b67d2697e8de29cfba07529f6d2aaadf8fd520c
sunil barewar <sunilvbarewar@gmail.com>
2024-01-07 13:28:23 +0530

    design changed

--- src/components/cart/EmptyCart.jsx
+++ src/components/cart/EmptyCart.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
   return (
-    <>
-      <h1 className="text-center text-5xl mb-4">Your Cart is empty!! </h1>
-      <h1 className="text-center text-[80px]">&#x1F610; </h1>
-    </>
+    <div className="flex justify-center items-center flex-col">
+      <div className="w-[250px]">
+        <img
+          src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-7359557-6024626.png"
+          alt=""
+        />
+      </div>
+      <h1 className="text-center text-3xl mb-4">Your Cart is empty!! </h1>
+      <Link
+        to={"/"}
+        href="#"
+        class="inline-flex text-white bg-primary hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+      >
+        Shop now
+      </Link>
+    </div>
   );
 };
 export default EmptyCart;
