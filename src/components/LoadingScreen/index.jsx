a6dca8f589d8a8e123fc010b5cabc9435b160c09
sunil barewar <sunilvbarewar@gmail.com>
2024-01-07 13:28:42 +0530

    created loader portal

--- src/components/LoadingScreen/index.jsx
+++ src/components/LoadingScreen/index.jsx
@@ -1,9 +1,60 @@
 import "./loading.css";
+import { createPortal } from "react-dom";
 const LoadingScreen = () => {
+  return <> {createPortal(<Loader />, document.getElementById("loader"))}</>;
+};
+
+const Loader = () => {
   return (
-    <div className="ring">
-      Loading
-      <span></span>
+    <div className="flex justify-center items-center min-h-[100vh]">
+      <svg class="pl" width="240" height="240" viewBox="0 0 240 240">
+        <circle
+          class="pl__ring pl__ring--a"
+          cx="120"
+          cy="120"
+          r="105"
+          fill="none"
+          stroke="#000"
+          stroke-width="20"
+          stroke-dasharray="0 660"
+          stroke-dashoffset="-330"
+          stroke-linecap="round"
+        ></circle>
+        <circle
+          class="pl__ring pl__ring--b"
+          cx="120"
+          cy="120"
+          r="35"
+          fill="none"
+          stroke="#000"
+          stroke-width="20"
+          stroke-dasharray="0 220"
+          stroke-dashoffset="-110"
+          stroke-linecap="round"
+        ></circle>
+        <circle
+          class="pl__ring pl__ring--c"
+          cx="85"
+          cy="120"
+          r="70"
+          fill="none"
+          stroke="#000"
+          stroke-width="20"
+          stroke-dasharray="0 440"
+          stroke-linecap="round"
+        ></circle>
+        <circle
+          class="pl__ring pl__ring--d"
+          cx="155"
+          cy="120"
+          r="70"
+          fill="none"
+          stroke="#000"
+          stroke-width="20"
+          stroke-dasharray="0 440"
+          stroke-linecap="round"
+        ></circle>
+      </svg>
     </div>
   );
 };
