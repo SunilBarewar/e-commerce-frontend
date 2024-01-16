fa89323c7b355108d933237b52325e0b7218cfbb
sunil barewar <sunilvbarewar@gmail.com>
2024-01-16 18:46:26 +0530

    alert for api response

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
+import LoadingScreen from "./LoadingScreen";
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const { auth } = useAuth();
@@ -21,7 +22,7 @@ const PersistLogin = () => {
     !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
   }, []);
 
-  return <>{isLoading ? <p>Loading</p> : <Outlet />}</>;
+  return <>{isLoading ? <LoadingScreen /> : <Outlet />}</>;
 };
 
 export default PersistLogin;
--- src/components/layouts/user/index.jsx
+++ src/components/layouts/user/index.jsx
@@ -4,6 +4,14 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import useAuth from "../../../hooks/useAuth";
 import useRefreshToken from "../../../hooks/useRefreshToken";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+} from "@chakra-ui/react";
+
 const Layout = () => {
   const { auth } = useAuth();
   const refresh = useRefreshToken();
@@ -21,7 +29,8 @@ const Layout = () => {
   }, []);
   return (
     <>
-      <div className="px-[12px] md:px-4 lg:px-[80px] font-Inter flex flex-col min-h-[100vh]">
+      <ApiAlert />
+      <div className="px-[12px] md:px-4 lg:px-[80px] font-Inter flex flex-col min-h-[100vh]  overflow-hidden">
         <Navbar />
         <div className="flex-grow">
           <Outlet />
@@ -32,4 +41,31 @@ const Layout = () => {
   );
 };
 
+const ApiAlert = () => {
+  const [alertVisible, setAlertVisibile] = useState(
+    JSON.parse(sessionStorage.getItem("alert"))
+  );
+
+  const handleAlertVisibility = () => {
+    setAlertVisibile("true");
+    sessionStorage.setItem("alert", "true");
+  };
+  return (
+    <>
+      {!alertVisible && (
+        <Alert status="error" style={{ position: "relative", zIndex: 10000 }}>
+          <AlertIcon />
+          <AlertTitle>
+            Initially API could take 60-90 seconds to Response.
+          </AlertTitle>
+          <AlertDescription>it's hosted on Render free plan</AlertDescription>
+          <CloseButton
+            style={{ position: "absolute", right: "1rem" }}
+            onClick={handleAlertVisibility}
+          />
+        </Alert>
+      )}
+    </>
+  );
+};
 export default Layout;
